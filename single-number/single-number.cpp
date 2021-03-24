4137e802c82c4f799ece78ebfa43937d112ef77f
Ethan Shayan Sarnevesht <35939705+shayans66@users.noreply.github.com>
2021-03-24 12:22:43 -0700

    Time: 32 ms (54.51%), Space: 20 MB (20.28%) - LeetHub

---
+++ single-number/single-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        unordered_map<int, int> map;
+        for(int i=0;i<nums.size();i++){
+
+            map[nums[i]]++;
+            
+        }
+        
+        for(auto it=map.begin();it!=map.end();it++){
+            if(it->second == 1)
+                return it->first;
+        }
+        
+        return -1;
+    }
+};
