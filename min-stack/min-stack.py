e3f6a522846d483c0c1f1be59f58b1d85c328808
shayans66 <35939705+shayans66@users.noreply.github.com>
2021-08-30 01:31:24 -0700

    Time: 88 ms (31.89%), Space: 19.2 MB (5.13%) - LeetHub

--- min-stack/min-stack.py
+++ min-stack/min-stack.py
@@ -1,23 +1,39 @@
 class MinStack:
+    
+    head = None
 
     def __init__(self):
         """
         initialize your data structure here.
         """
-        self.arr = []
 
     def push(self, val: int) -> None:
-        self.arr.append(val)
-
+        if self.head is None:
+            self.head = Node(val, val, None)
+        else:
+            # self.head.next = Node(val, min(self.head.val, val), None)
+            print(self.head.val, val, min(self.head.val, val),end=' ')
+            print('be4',self.head.min,end=' ')
+            self.head = Node(val, min(self.head.min, val), self.head)
+            print('aftr',self.head.min)
+        
     def pop(self) -> None:
-        self.arr.pop()
+        self.head = self.head.next
 
     def top(self) -> int:
-        return self.arr[-1]
+        return self.head.val
+
 
     def getMin(self) -> int:
-        return min(self.arr)
+        return self.head.min
 
+
+class Node:
+    def __init__(self,val,min,next):
+        self.val = val
+        self.min = min
+        self.next = next
+            
 
 # Your MinStack object will be instantiated and called as such:
 # obj = MinStack()
