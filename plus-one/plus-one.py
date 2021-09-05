066879fc55f9642edf50f00f2c738f4c076bd68e
shayans66 <35939705+shayans66@users.noreply.github.com>
2021-09-05 16:22:49 -0700

    Time: 32 ms (70.24%), Space: 14.2 MB (75.53%) - LeetHub

--- plus-one/plus-one.py
+++ plus-one/plus-one.py
@@ -9,4 +9,45 @@ class Solution:
             
 #         return digits
 
-        return list(str(int(''.join([str(x) for x in digits])) + 1))
+        # return list(str(int(''.join([str(x) for x in digits])) + 1))
+    
+
+#         r = len(digits)-1
+#         if digits[r]+1 >= 10:
+#             digits.append((digits[r]+1)%10)
+#             digits[r] = 1
+#             r -= 1
+
+#             while r >= 0:
+#                 if digits[r]+1 >= 10:
+#                     digits[r] = (digits[r]+1)%10
+#                     r -= 1
+#                 else:
+#                     digits[r] += 1
+            
+#         else:
+#             digits[r] += 1
+            
+#         return digits
+
+        r = len(digits)-1
+#         if digits[r] + 1 < 10:
+#             return digits[0:r-1] + [digits[r]+1]
+#         else:
+            
+        digits[r] += 1
+        while digits[r] >= 10:
+            # if nums[r]+1 < 10:
+            #     nums[r]+=1
+            #     break
+            # else:
+            #     nums[r] = (nums[r]+1) % 10
+            digits[r] %= 10
+            r -= 1
+            if r >= 0:
+                digits[r] += 1
+            else:
+                digits.insert(0, 1)
+        return digits
+                
+                
