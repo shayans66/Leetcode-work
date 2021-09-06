b787e9b743674559d36c9b34075359c51eb51b90
shayans66 <35939705+shayans66@users.noreply.github.com>
2021-09-05 17:38:19 -0700

    Time: 44 ms (62.93%), Space: 15.1 MB (24.90%) - LeetHub

---
+++ fizz-buzz/fizz-buzz.py
@@ -0,0 +1,3 @@
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        return ["FizzBuzz" if (x%3==0 and x%5==0) else "Fizz" if (x%3==0) else 'Buzz' if (x%5==0) else str(x) for x in range(1, n+1)]
