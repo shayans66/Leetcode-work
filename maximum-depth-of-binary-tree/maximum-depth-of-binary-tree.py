e1277a6b3069ae12bd17b7d1b66cce0406ff41f6
shayans66 <35939705+shayans66@users.noreply.github.com>
2021-10-06 23:42:55 -0700

    Time: 44 ms (57.53%), Space: 16.1 MB (40.87%) - LeetHub

--- maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.py
+++ maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.py
@@ -7,33 +7,32 @@
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
         
-        maxlen = curlen = 1 if root else 0
-        stack = []
+#         maxlen = curlen = 1 if root else 0
+#         stack = []
         
-        cur = root
+#         cur = root
         
-        while cur or stack:
-            if cur.left:
-                
-                
-                if cur.right:
-                    stack.append([cur.right, curlen+1])
-                cur = cur.left
-                curlen += 1
-                maxlen = max(maxlen, curlen)
-            elif cur.right:
-                cur = cur.right
-                curlen += 1
-                maxlen = max(maxlen, curlen)
-                
-                
-            else:
-                maxlen = max(maxlen, curlen)
-                if stack: 
-                    tup = stack.pop()
-                    cur = tup[0]
-                    curlen = tup[1]
-                else: cur = None
+#         while cur or stack:
+#             if cur.left:
+#                 if cur.right:
+#                     stack.append([cur.right, curlen+1])
+#                 cur = cur.left
+#                 curlen += 1
+#                 maxlen = max(maxlen, curlen)
+#             elif cur.right:
+#                 cur = cur.right
+#                 curlen += 1
+#                 maxlen = max(maxlen, curlen)
+#             else:
+#                 maxlen = max(maxlen, curlen)
+#                 if stack: 
+#                     tup = stack.pop()
+#                     cur = tup[0]
+#                     curlen = tup[1]
+#                 else: cur = None
             
-        return maxlen
+#         return maxlen
                     
+        if not root:
+            return 0
+        return 1+max(self.maxDepth(root.left), self.maxDepth(root.right))
