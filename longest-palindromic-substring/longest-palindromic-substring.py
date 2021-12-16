bd7df7a35ab09c0509196a158d6990ddee7d222d
shayans66 <35939705+shayans66@users.noreply.github.com>
2021-12-15 20:01:13 -0800

    Time: 1604 ms (37.38%), Space: 14.1 MB (93.64%) - LeetHub

--- longest-palindromic-substring/longest-palindromic-substring.py
+++ longest-palindromic-substring/longest-palindromic-substring.py
@@ -12,7 +12,7 @@ class Solution:
         res,resLen = '',0
         # l,r = 0, len(s)-1
         # print(s)
-        for i in range(0,len(s)):
+        for i in range(len(s)):
             #odd
             l,r = i,i
             # print(l,r,s[l],s[r])
